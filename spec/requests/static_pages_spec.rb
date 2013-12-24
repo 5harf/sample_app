a7a7b8539be85ad03fe642dfe79fc6f9c6e0db24
Jack <jsharf212@gmail.com>
2013-12-24 02:02:26 -0800

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,6 +4,18 @@ describe "Static pages" do
 
   describe "Home page" do
 
+    it "should have the right title" do 
+      visit '/static_pages/home'
+      page.should have_selector('title', 
+        :text => "| Home")
+    end
+
+    it "should have the header 'sample app'" do
+      visit "/static_pages/home"
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
@@ -11,9 +23,43 @@ describe "Static pages" do
   end
 
   describe "Help page" do
+
+    it "should have the right title" do
+      visit "/static_pages/help"
+      page.should have_selector('title',
+        :text => "| Help")
+    end
+
+    it "should have the header 'Help Page'" do 
+      visit "/static_pages/help"
+      page.should have_selector('h1', 
+        :text => 'Help')
+    end
+
   	it "should have the content 'Help'" do
   		visit '/static_pages/help'
   		page.should have_content('Help')
   	end
   end
+
+  describe "About page" do 
+
+    it "should have the right title" do
+      visit "/static_pages/about"
+      page.should have_selector('title', 
+        :text => "| About Us")
+    end
+
+    it "should have the header 'About Page'" do
+      visit "/static_pages/about"
+      page.should have_selector('h1',
+        :text => "About Us")
+end
+    it "should have the content 'About us'" do
+    visit '/static_pages/about'
+    page.should have_content('About Us')
+    end
+  end
+
+
 end
