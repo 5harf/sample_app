3c0714171b41da5520d1ecf1ab03046434f14cac
Jack <jsharf212@gmail.com>
2014-01-08 11:40:03 -0800

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,14 +3,25 @@ require 'spec_helper'
 
 describe "Static pages" do
   subject {page}
+
+  shared_examples_for "all static pages" do
+    it { should have_selector 'title', text: full_title(page_title)}
+    it { should have_selector 'h1', text: heading}
+  end
+
+
   describe "Contact page" do
     
+
+
     before {visit contact_path}
 
-    it {should have_selector('title', text: full_title('Contact Us')) }
-    
+    let(:heading) {'Contact Us'}
+    let(:page_title) {''}
 
-    it {should have_selector('h1', text: 'Contact Us')}
+    it_should_behave_like "all static pages"
+
+
     
 
     it {should have_content('joe@schmo.com')}
@@ -20,12 +31,12 @@ describe "Static pages" do
 
   describe "Home page" do
     before {visit root_path}
+    let(:heading) {'Home'}
+    let(:page_title) {''}
     
     it { should_not have_selector 'title' , :text => 'Home' }
 
-    it { should have_selector('title', text: full_title('')) }
-  
-    it { should have_selector('h1', :text => 'Sample App')}
+   
     
     it { should have_content('Sample App') }
     
@@ -33,11 +44,10 @@ describe "Static pages" do
 
   describe "Help page" do
     before {visit help_path}
-    
-    it {should have_selector('title', text: full_title('Help'))}
+    let(:heading) {'Help'}
+    let(:page_title) {''}
     
 
-    it {should have_selector('h1', :text => 'Help')}
     
 
   	it {should have_content('Help')}
@@ -46,12 +56,27 @@ describe "Static pages" do
 
   describe "About page" do 
     before {visit about_path}
-    
-    it {should have_selector('title', text: full_title('About Us'))}
+    let(:heading) {About}
+    let(:title) {''}
 
-    it {should have_selector('h1', :text => "About Us")}
 
     it {should have_content('About Us')}
+
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: 'About'
+    click_link "Help"
+    page.should have_selector 'title', text: 'Help'
+    click_link "Contact"
+    page.should have_selector 'title', text: 'Contact'
+    click_link "Home"
+    click_link "Sign Up Now!"
+    page.should have_selector 'title', text: 'Sign up'
+    click_link "sample app"
+    page.should_not have_selector 'title', text: 'Sign up'
 
   end
 
